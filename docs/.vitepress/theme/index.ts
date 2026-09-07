9bbf99bb7dfcd2358b41169a9185e92a67ef072a
kovawx <kovawx@gmail.com>
2026-09-07 11:50:26 +0800

    feat(docs): add interactive ZNet Sink client tour

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,6 +1,7 @@
 import DefaultTheme from 'vitepress/theme'
 import type { Theme } from 'vitepress'
 import DiscussionFeed from './components/DiscussionFeed.vue'
+import ClientTour from './components/ClientTour.vue'
 import DownloadChooser from './components/DownloadChooser.vue'
 import ProjectCatalog from './components/ProjectCatalog.vue'
 import ProjectMeta from './components/ProjectMeta.vue'
@@ -12,6 +13,7 @@ export default {
   Layout,
   enhanceApp({ app }) {
     app.component('DiscussionFeed', DiscussionFeed)
+    app.component('ClientTour', ClientTour)
     app.component('DownloadChooser', DownloadChooser)
     app.component('ProjectCatalog', ProjectCatalog)
     app.component('ProjectMeta', ProjectMeta)
